8a614c7456f8ab2c5ee56e35cfbab629a0519b66
HyeranPark99 <bhyan14@gmail.com>
2022-01-27 23:18:42 -0500

    fav Working but Zipcode is not working

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProfilePage from './ProfilePage';
+import ProfileHelper from './ProfileHelper';
+
 
 
 //Should be passed down the user id from login page when 
@@ -9,38 +11,24 @@ export default function Profile() {
   const[userData, setUserData] = useState([
     {zipCode: 20001}
   ])
+
+   let id = localStorage.getItem('email')
+   console.log(id)
+    
   const [weatherData2, setWeatherData2] = useState([])
   
     console.log(userData[0].zipCode)
-    const newZip = userData[0].zipCode 
+    // const newZip = userData[0].zipCode 
+    const newZip = localStorage.getItem('zipcode')
 
-    // useEffect(() => {
-    //   async function getFavorites(){
-    //     const res= await axios.get('https://pacific-taiga-17233.herokuapp.com/api/favorites/')       
-    //       setUserData(res.data); 
-    //        console.log(res)      
-    //   }
-    //   getFavorites();
-    
-    // },[])
-
-    // useEffect(() => {
-    
-    //   async function callFavorites(){        
-    //         const results =  await axios(`http://api.openweathermap.org/data/2.5/weather?zip=${newZip}&units=imperial&appid=${apiKey}`)
-    //         setWeatherData2(results.data);
-      
-    //     }    
-    //   callFavorites();
-    // },[])
   const apiKey = '9d5b71c06e78dc59f8f6f2102a0bf72b'
   useEffect(() => {
-    async function getFavorites(){
-      const res= await axios.get('https://pacific-taiga-17233.herokuapp.com/api/favorites/')       
-        setUserData(res.data); 
-         console.log(res)      
-    }
-    getFavorites();
+    // async function getFavorites(){
+    //   const res= await axios.get('https://pacific-taiga-17233.herokuapp.com/api/favorites/')       
+    //     setUserData(res.data); 
+    //      console.log(res)      
+    // }
+    // getFavorites();
   
     async function callFavorites(){        
           const results =  await axios(`http://api.openweathermap.org/data/2.5/weather?zip=${newZip}&units=imperial&appid=${apiKey}`)
@@ -49,11 +37,12 @@ export default function Profile() {
       }    
     callFavorites();
   },[])
-   
+  ProfileHelper();
 
   return(
     <div>
       <ProfilePage weatherData2={weatherData2}/>
+      {/* <ProfileHelper /> */}
     </div>
   )  
 }
---
+++ src/components/Profile/ProfileHelper.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function ProfileHelper() {
+
+    let email = localStorage.getItem('email')
+    console.log(email)
+
+    const[users, setUsers] = useState([])
+
+    useEffect(()=> {
+       try{
+        async function getUsers(){
+            const res= await axios.get('https://pacific-taiga-17233.herokuapp.com/api/users/')       
+              setUsers(res.data); 
+               console.log(res)      
+          }
+          getUsers();
+          
+        }catch(error){
+         throw error.message   
+        }
+
+
+    },[])
+
+    console.log(users)
+
+    
+
+    // if(email !== "" && users){
+    // const findId = users.filter(user => user.email === email)   
+    // const val =  findId[0].id               
+    // console.log(findId[0].id)
+    // localStorage.setItem('id',val )
+    // }
+
+    let filteredId = localStorage.getItem('id')
+     
+   async function getResp(){
+    const resp= await axios.get(`https://pacific-taiga-17233.herokuapp.com/api/favorites/${filteredId}`); 
+    const result = resp.data
+    console.log(result)
+    localStorage.setItem('zipcode',result[0].zipCode) 
+   } 
+   getResp();
+
+   const zipResult = localStorage.getItem('zipcode')
+   console.log(zipResult)
+
+  return <div></div>;
+}
