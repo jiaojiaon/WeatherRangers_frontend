031c5c1be4293afaa02243d3ff6ae8f37fc006e4
Farai Major <faraimutukumira@gmail.com>
2022-02-03 15:28:30 -0500

    error fix

--- src/components/landing/WeatherAndForecast.jsx
+++ src/components/landing/WeatherAndForecast.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Weather from "./Weather";
-import Forecast from "./forecast";
+import Forecast from "./Forecast";
 
 import "../../styles/WeatherAndForecast.css";
 
